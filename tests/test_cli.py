ecef78f85ba912b71fdfc88af307d73859825eff
M1RRoN <mds.myst@gmail.com>
2022-10-26 16:05:06 +0400

    step 1

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,17 +1,11 @@
 import os
 
-import pytest
-
-from page_loader import cli
-
 
 def test_help_command():
     exit_status = os.system('page-loader -h')
     assert exit_status == 0
 
 
-def test_cli_without_args():
-    with pytest.raises(SystemExit) as error:
-        cli.parse_arguments()()
-    assert error.type == SystemExit
-    assert error.value.code == 2
+def read_file(path):
+    with open(path, 'r') as file:
+        return file.read()
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,6 +1,6 @@
 import pytest
-from page_loader.loader import site_name
+from page_loader.rename import get_rename
 
 
-def test_site_name():
-    assert site_name('https://ru.hexlet.io/courses') == 'ru-hexlet-io-courses.html'
+def test_rename():
+    assert get_rename('https://ru.hexlet.io/courses') == 'ru-hexlet-io-courses.html'
